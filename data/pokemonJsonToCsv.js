6e44e4e8ab0c83f4882d6520bd658a17fdb8ff9e
ZacharyFarrell <162598563+ZacharyFarrell@users.noreply.github.com>
2024-06-10 02:58:01 -0500

    Oops I forgot Generation

--- data/pokemonJsonToCsv.js
+++ data/pokemonJsonToCsv.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./pokemon.json'));
 
-let file = '#,Pokemon,Type 1,Type 2,Total,HP,Attack,Defense,Special Attack,Special Defense,Speed,Legendary,Evolves Into';
-for (const { id, pokemon, type1, type2, total, hp, attack, defense, specialAttack, specialDefense, speed, isLegendary, evolvesInto } of data) {
-  file += `\n${id},${pokemon},${type1},${type2 || ''},${total},${hp},${attack},${defense},${specialAttack},${specialDefense},${speed},${isLegendary},"${evolvesInto}"`;
+let file = '#,Pokemon,Type 1,Type 2,Total,HP,Attack,Defense,Special Attack,Special Defense,Speed,Legendary,Generation,Evolves Into';
+for (const { id, pokemon, type1, type2, total, hp, attack, defense, specialAttack, specialDefense, speed, isLegendary, generation, evolvesInto } of data) {
+  file += `\n${id},${pokemon},${type1},${type2 || ''},${total},${hp},${attack},${defense},${specialAttack},${specialDefense},${speed},${isLegendary},${generation},"${evolvesInto}"`;
 }
 fs.writeFileSync('./pokemon.csv', file);
